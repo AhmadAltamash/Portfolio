786bfa61b70ae719ffa9d0ef28d7f05f8c3bd02b
Altamash Ahmad <altamashahmad910@gmail.com>
2025-01-19 01:08:48 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Hero, Navbar } from './sections'
+import { About, Hero, Navbar } from './sections'
 
 const App = () => {
   return (
     <main className='max-w-7xl mx-auto'>
       <Navbar/>
       <Hero/>
+      <About/>
     </main>
   )
 }
--- src/sections/About/index.jsx
+++ src/sections/About/index.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className='c-space my-20'>
+        <div className='grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full'>
+            <div className='col-span-1 xl:row-span-3'>
+                <div className='grid-container'>
+                    <img src="/assets/grid1.png" alt="grid-1" className='w-full sm:h-[276px] h-fit object-contain' />
+                    <div>
+                        <p className='grid-headtext'>Hi, I am Altamash</p>
+                        <p className='grid-subtext'>With a year of experience, I have honed my skills in frontend and backend development, with a focus on user friendly and astonishing animated and 3D websites.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className='col-span-1 xl:row-span-3'>
+                <div className='grid-container'>
+                    <img src="/assets/grid-2.png" alt="grid-2" className='w-full sm:h-[276px] h-fit object-contain'/>
+                    <div>
+                        <p className='grid-headtext'>Tech Stack</p>
+                        <p className='grid-subtext'>I specialize in JavaScript/TypeScript with a focus on React and Next.js ecosystems.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
   )
 }
 
