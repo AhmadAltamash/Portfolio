dc47f2ba01768a2397c0b6e4a99bbd3c539e9f9b
Altamash Ahmad <altamashahmad910@gmail.com>
2025-01-25 22:52:28 +0530

    update

--- src/sections/About/index.jsx
+++ src/sections/About/index.jsx
@@ -30,6 +30,11 @@ const About = () => {
                 </div>
             </div>
             
+            <div className='col-span-1'>
+                <div className='grid-container'>
+                    <img src="/assets/grid3.png" alt="grid-3" className='w-full sm:h-[276px] h-fit object-contain'/>
+                </div>
+            </div>
         </div>
     </section>
   )
