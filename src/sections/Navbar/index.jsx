99293bf0c0854457491b5fc461ac0d7d9e9e4955
Altamash Ahmad <altamashahmad910@gmail.com>
2025-02-01 22:57:17 +0530

    update

--- src/sections/Navbar/index.jsx
+++ src/sections/Navbar/index.jsx
@@ -23,8 +23,8 @@ const Navbar = () => {
     <header className='fixed top-0 left-0 right-0 z-50 bg-black/90'>
         <div className='max-w-7xl mx-auto'>
             <div className='flex justify-between items-center py-5 mx-auto c-space'>
-               <a href="/" className='hover:bg-slate-600/20 rounded-full transition-all duration-300 hover:overflow-hidden ease'>
-                <img src={"assets/Logo.svg"} alt="logo" className='sm:h-[5rem] h-[2.5rem]'/>
+               <a href="/" className='hover:scale-110 transition-all duration-300 hover:overflow-hidden ease'>
+                <img src={"/Logo.svg"} alt="logo" className='sm:h-[1.7rem] h-[1.1rem]'/>
                </a>
                <button onClick={toggleMenu} className='text-neutral-400 hover:text-white focus:outline-none sm:hidden flex' aria-label='Toggle Menu'>
                 <img src={isOpen ? "assets/close.svg" : "assets/menu.svg"} alt="toggle" className='w-6 h-6'/>
