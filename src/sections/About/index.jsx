eb80adb3cecf6c39f61724278a06210ba9116eed
Altamash Ahmad <altamashahmad910@gmail.com>
2025-02-04 20:50:37 +0530

    update

--- src/sections/About/index.jsx
+++ src/sections/About/index.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Globe from 'react-globe.gl'
+import { color } from 'three/tsl'
+import { Button } from '../../components'
 
 const About = () => {
+
+    const [hasCopied, setHasCopied] = useState(false)
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText('work.altamashahmad@gmail.com');
+        setHasCopied(true);
+        setTimeout(() => {
+            setHasCopied(false);
+        }, 2000)
+    }
+
   return (
     <section className='c-space my-20'>
-        <div className='grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full'>
-            <div className='col-span-1 xl:row-span-3'>
-                <div className='grid-container'>
+        <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
+            <div className="col-span-1 xl:row-span-3">
+                <div className="grid-container">
                     <img src="/assets/grid1.png" alt="grid-1" className='w-full sm:h-[276px] h-fit object-contain' />
                     <div>
                         <p className='grid-headtext'>Hi, I am Altamash</p>
@@ -19,20 +33,61 @@ const About = () => {
                     <img src="/assets/grid-2.png" alt="grid-2" className='w-full sm:h-[276px] h-fit object-contain'/>
                     <div>
                         <p className='grid-headtext'>Tech Stack</p>
-                        <p className='grid-subtext'>I specialize in JavaScript/TypeScript with a focus on React and Next.js ecosystems.</p>
+                        <p className='grid-subtext'>I specialize in JavaScript/TypeScript with a focus on React and Next.js ecosystems. Additionally, I have experience working with Docker and Jenkins. I am proficient in MongoDB, PostgreSQL, and Firebase for data management. And, I have a strong grasp of REST APIs, GraphQL, WebSockets and Web3.
+                        </p>
                     </div>
                 </div>
             </div>
 
-            <div className='col-span-1'>
+            <div className='col-span-1 xl:row-span-4'>
                 <div className='grid-container'>
-                    <img src="/assets/grid3.png" alt="grid-3" className='w-full sm:h-[276px] h-fit object-contain'/>
+                    <div className='rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center'>
+                        <Globe
+                            height={326}
+                            width={326}
+                            backgroundColor='rgba(0,0,0,0)'
+                            backgrroundImageOpacity={0.5}
+                            showAtmosphere
+                            showGraticules
+                            globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+                            bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+                            labelsData={[{
+                                lat: 25.642216, lng: 85.105396,
+                                text: "I'm here!",
+                                color: 'white',
+                                size: 20,
+                            }]}
+                        />
+                    </div>
+                    <div>
+                        <p className='grid-headtext'>I can work remotely across most timezones.</p>
+                        <p className='grid-subtext'>I'm based in Patna, Bihar. With remote work available.</p>
+                        <Button name="contact" isBeam containerClass="w-full mt-10" onClick={handleCopy}/>
+                    </div>
                 </div>
             </div>
-            
-            <div className='col-span-1'>
+
+            <div className='xl:col-span-2 xl:row-span-3'>
                 <div className='grid-container'>
-                    <img src="/assets/grid4.png" alt="grid-4" className='w-full sm:h-[276px] h-fit object-contain'/>
+                    <img src="/assets/grid3.png" alt="grid-3" className='w-full sm:h-[266px] h-fit object-contain'/>
+                    <div>
+                        <p className='grid-headtext'>My Passion for Coding</p>
+                        <p className='grid-subtext'>I love solving problems and building things through code. Coding isn't just my profession - it is my passion.
+                        Eager to learn new Languages and Framework, Expertise in making User Friendly Software/Websites.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className='xl:col-span-1 xl:row-span-2'>
+                <div className='grid-container'>    <img src="/assets/grid4.png" alt="grid-4" className='w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top' />
+                <div className='space-y-2'>
+                    <p className='grid-subtext text-center'>Contact me</p>
+                    <div className='copy-container' onClick={handleCopy}>
+                        <img src={hasCopied ? "assets/tick.svg" : "assets/copy.svg"} alt="copy" />
+                        <p className='lg:text-xl md:text-lg font-medium text-gray_gradient text-white'>work.altamashahmad@gmail.com</p>
+                    </div>
+                </div>
                 </div>
             </div>
         </div>
